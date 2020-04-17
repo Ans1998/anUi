d8ce1c25d5f44bd892b1c7b24d7e809802f63a6f
An <1633004849@qq.com>
2020-04-17 11:00:31 +0800

    storybook

---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { action } from '@storybook/addon-actions'
+import Button from "../components/Button/index"
+import {storiesOf} from "@storybook/react";
+
+const styles: React.CSSProperties = {
+    textAlign: "center"
+};
+
+const defaultButton = () => {
+    return <div style={{margin: 10}}>
+        <Button onClick={action('click')}>default Button</Button>
+    </div>
+};
+const buttonWithSize = () => {
+  return <div style={{margin: 10}}>
+      <div>
+          <Button size="lg">lg Button</Button>
+      </div>
+     <div style={{marginTop: 10}}>
+         <Button size="sm">sm Button</Button>
+     </div>
+  </div>
+};
+const buttonWithType = () => (
+    <div style={{margin: 10}}>
+        <div>
+            <Button btnType="primary"> primary button </Button>
+        </div>
+        <div style={{marginTop: 10}}>
+            <Button btnType="danger"> danger button </Button>
+        </div>
+        <div style={{marginTop: 10}}>
+            <Button btnType="link" href="https://google.com"> link button </Button>
+        </div>
+    </div>
+);
+storiesOf('Button 组件', module)
+    .add('默认 Button', defaultButton)
+    .add('不同尺寸 Button', buttonWithSize)
+    .add("不同type Button", buttonWithType)
+;
---
+++ src/stories/index.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Button } from '@storybook/react/demo';
+import '../styles/index.scss'
+
+export default { title: '首页' };
+
+// export const withText = () => <Button>Hello Button</Button>;
+
+export const Home = () => (
+    <div>
+        欢迎使用AnUi组件
+    </div>
+);
